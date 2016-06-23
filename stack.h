f1358fe9e94aeb4a3a885580cd7199b640b55a2c
Eddie Dinkz <codemaster.eddie@gmail.com>
2016-06-23 18:55:39 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -5,8 +5,8 @@
 #define PUSH_FAIL 0
 
 typedef struct stack_lnklst {
-	int key; // key of the stack element
-	struct stack_lnklst *next;  // next element
+  int key; // key of the stack element
+  struct stack_lnklst *next;  // next element
 } Stack;
 
 Stack *initializeStack(void);
